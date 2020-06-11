d6c42a1d34d79f05f3a47ac6f2eebe486d87aa06
usaurabh <udit.saurabh@dxc.com>
2020-06-11 09:27:57 +0530

    firstcommit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./homepage.styles.scss";
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HATS</div>
+            <div className="subtitle">SHOPNOW</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
